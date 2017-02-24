3891d5cda62de23cbf761ab239365775125ff227
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-02-24 13:41:48 -0600

    working on ng-route

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
 "use strict";
 
 var app = angular.module("wideWorldApp", []);
+
+
+// app.config(function($routeProvider) {
+//     $routeProvider
+//     .when("/", {
+//         templateUrl: "partials/guide-cards.html",
+//         controller: "UserCtrl"
+//     });
+// });
